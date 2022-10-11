3c09e4f3cfa00883fb36ba64021c0080e9415caa
chester <chsieh5@hawk.iit.edu>
2022-10-11 23:36:59 +0800

    model file

--- playground/src/bin/wasm_exp.rs
+++ playground/src/bin/wasm_exp.rs
@@ -1,24 +1,18 @@
 use anyhow::{bail, Result};
 use tch::vision::imagenet;
 
-pub fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
-    let (model_file, image_file) = match args.as_slice() {
-        [_, m, i] => (m.to_owned(), i.to_owned()),
-        _ => bail!("usage: main model.pt image.jpg"),
-    };
-    // Load the image file and resize it to the usual imagenet dimension of 224x224.
-    let image = imagenet::load_image_and_resize224(image_file)?;
-
-    // Load the Python saved module.
+pub fn main() -> Result<String> {
+    let image_file = "/Users/chester/PycharmProjects/Gobblet/playground/img/img.png".to_owned();
+    let model_file = "/Users/chester/PycharmProjects/Gobblet/wasm_lab/model.pt".to_owned();
+    let image = imagenet::load_image_and_resize(image_file,224,224)?;
     let model = tch::CModule::load(model_file)?;
-
-    // Apply the forward pass of the model to get the logits.
-    let output = image.unsqueeze(0).apply(&model).softmax(-1, tch::Kind::Float);
-
-    // Print the top 5 categories for this image.
-    for (probability, class) in imagenet::top(&output, 5).iter() {
-        println!("{:50} {:5.2}%", class, 100.0 * probability)
-    }
-    Ok(())
+    let output = model.forward_ts(&[image.unsqueeze(0)])?.softmax(-1,tch::Kind::Float);
+    let binding = imagenet::top(&output, 5)
+        .iter()
+        .map(|(probability, class)| format!("{:50} {:5.2}%", class, 100.0 * probability))
+        .collect::<Vec<_>>()
+        .join(" ");
+    Ok(binding)
 }
+
+
--- wasm_lab/src/lib.rs
+++ wasm_lab/src/lib.rs
@@ -1,11 +1,20 @@
 use wasm_bindgen::prelude::*;
+use tch::vision::imagenet;
+use anyhow::{bail, Result};
+use tch::*;
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
+pub fn input() -> anyhow::Result<String> {
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    let image_file = "/Users/chester/PycharmProjects/Gobblet/playground/img/img.png".to_owned();
+    let model_file = "/Users/chester/PycharmProjects/Gobblet/wasm_lab/model.pt".to_owned();
+    let image = imagenet::load_image_and_resize(image_file,224,224)?;
+    let model = tch::CModule::load(model_file)?;
+    let output = model.forward_ts(&[image.unsqueeze(0)])?.softmax(-1,tch::Kind::Float);
+    let binding = imagenet::top(&output, 5)
+        .iter()
+        .map(|(probability, class)| format!("{:50} {:5.2}%", class, 100.0 * probability))
+        .collect::<Vec<_>>()
+        .join(" ");
+    Ok(binding)
 }
